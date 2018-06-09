0444518a5808de0cf4983da28b3ba2d7f90b469f
Andreas Wilcox <UnknownBum@gmail.com>
2018-06-09 17:56:20 +0200

    Added plane player setup

---
+++ Assets/FlightControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FlightControls : MonoBehaviour
+{
+    public float Speed;
+
+    public void Start()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        var pitch = Input.GetAxis("Pitch");
+        var yaw = Input.GetAxis("Yaw") * 0.5f;
+        var roll = Input.GetAxis("Roll");
+        transform.Rotate(pitch, yaw, roll);
+
+        transform.Translate(0, 0, Speed, Space.Self);
+    }
+}
