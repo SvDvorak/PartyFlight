2d834f9dc095b3fa97293c92a8c516ea8620c7f4
Andreas Wilcox <UnknownBum@gmail.com>
2018-06-09 18:56:21 +0200

    Added package dropping

--- Assets/FlightControls.cs
+++ Assets/FlightControls.cs
@@ -3,10 +3,32 @@
 public class FlightControls : MonoBehaviour
 {
     public float Speed;
+    public GameObject TargetReticule;
+    public GameObject PackageTemplate;
+    public Transform DropPoint;
 
     public void Start()
     {
 
+    }
+
+    public void Update()
+    {
+        RaycastHit hitInfo;
+        var ray = new Ray(transform.position, transform.rotation * new Vector3(0, -1, 1));
+        Debug.DrawRay(ray.origin, ray.direction*2, Color.red);
+        var hasHitGround = Physics.Raycast(ray, out hitInfo);
+        if (hasHitGround)
+        {
+            TargetReticule.transform.position = hitInfo.point;
+        }
+
+        if (Input.GetButtonDown("Drop"))
+        {
+            var package = Instantiate(PackageTemplate, DropPoint.position, Random.rotation, transform.parent);
+            package.GetComponent<PackageFlight>().Target = TargetReticule.transform.position;
+            Debug.Log("Dropped");
+        }
     }
 
     public void FixedUpdate()
---
+++ Assets/PackageFlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PackageFlight : MonoBehaviour
+{
+    public Vector3 Target;
+    public float BaseSpeed;
+    public float Acceleration;
+    private float _currentSpeed;
+
+    public void Start()
+    {
+        _currentSpeed = BaseSpeed;
+    }
+
+    public void Update()
+    {
+        var toTarget = Target - transform.position;
+        if (toTarget.sqrMagnitude > _currentSpeed*_currentSpeed)
+        {
+            _currentSpeed += Acceleration * Time.deltaTime;
+            transform.position += toTarget.normalized * _currentSpeed;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
